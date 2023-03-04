e6f448ca3ad9b90792fca2fb4d45d3383cce1834
Martin Tashkoski <tashkoskim@yahoo.com>
2023-03-04 05:00:21 +0100

    Add project files.

---
+++ HanoiTower/Disk.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanoiTower
+{
+    public class Disk
+    {
+        private int _size = 1;
+        private string _disk;
+        public Disk(int size)
+        {
+            if(size<0)
+            {
+                Size = Math.Abs(size);
+            }
+            else if (size == 0)
+            {
+                Size = 1;
+            }
+
+            Size = size;
+            GetDisk = createDisk();
+        }
+
+        public int Size { get => _size; set => _size = value; }
+        public string GetDisk { get => _disk; set => _disk = value; }
+
+        private string createDisk()
+        {
+            string peg = "«";
+
+            for (int i=0;i < Size - 1; i++)
+            {
+                peg += "=";
+            }
+
+            peg += "¦";
+
+            for (int i = 0; i < Size - 1; i++)
+            {
+                peg += "=";
+            }
+
+            peg += "»";
+
+            return peg;
+        }
+    }
+}
---
+++ HanoiTower/TowerOfHanoi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanoiTower
+{
+    public class TowerOfHanoi
+    {
+        public static void Play(int noDisks)
+        {
+            if(noDisks > 0)
+            {
+                TheGame();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. The number of disks n must be n > 0!");
+            }
+        }
+
+        private static void TheGame()
+        {
+
+        }
+    }
+}
